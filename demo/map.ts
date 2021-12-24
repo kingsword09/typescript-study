bc343d67da0c1c390c3894f5f5eb14680262b32d
kingsword09 <kingsword09@users.noreply.github.com>
2021-12-24 15:06:17 +0800

    Update map.ts

--- demo/map.ts
+++ demo/map.ts
@@ -1,7 +1,7 @@
 interface IMap<K, V> {
     clear(): void;
     delete(key: K): boolean;
-    forEach(callbackfn: (value: V, key: K, map: Map<K, V>) => void, thisArg?: any): void;
+    forEach(callbackfn: (value: V, key: K, map: IMap<K, V>) => void, thisArg?: any): void;
     get(key: K): V | undefined;
     has(key: K): boolean;
     set(key: K, value: V): boolean;
